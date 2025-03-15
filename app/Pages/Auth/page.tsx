ad5712ebe27542f6fdc94a33b323fdd73dfea5f2
DewaPermana22 <dewarisa77@gmail.com>
2025-03-15 12:37:46 +0700

    Client : Create a Wireframe For Auth Pages

--- app/Pages/Auth/page.tsx
+++ app/Pages/Auth/page.tsx
@@ -1,11 +1,24 @@
+import TextComponent from '@/app/components/Atoms/Text'
+import { FormLogin } from '@/app/components/ui/FormLogin'
+import Form from 'antd/es/form/Form'
+import Image from 'next/image'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page
-        {/* <button onClick={redirect('../Main')} className='h-7 w-3xl bg-amber-300 text-2xl text-white'>Goto Main</button> */}
+    <>
+    <div className='flex bg-white w-full h-screen'>
+      <div className=''>
+      <Image src={"/AuthImage.svg"} alt="Logo" width={650} height={1000} className="transition-all ease-in-out duration-300" />
+      </div>
+      <div className='flex flex-col justify-center pl-20'>
+        <TextComponent text='Welcome Back' type='subtitle' className='text-text' />
+        <TextComponent text='Smart HR solutions for better employee management.' type='paragraph' className='text-text' />
+        <FormLogin/>
+      </div>
     </div>
+    </>
   )
 }
 
